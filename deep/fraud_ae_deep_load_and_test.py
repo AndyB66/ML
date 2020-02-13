8ea4771f2319d5c678abd0a50c4bf19c28e9a481
Andrzej Bobyk <andrzej.bobyk@gmail.com>
2020-02-13 20:09:28 +0100

    Created boxplots

--- deep/fraud_ae_deep_load_and_test.py
+++ deep/fraud_ae_deep_load_and_test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras import backend as K
 from keras.models import Model, load_model
-from keras.losses import mean_squared_error, binary_crossentropy
+from keras.losses import binary_crossentropy
 from ae_lib import iRprop_
 
 train_data_path = 'fraud_train_optimal.pickle'
@@ -12,10 +12,12 @@ test_data_path = 'fraud_test_optimal.pickle'
 encoder_path = 'fraud_encoder_deep_optimal.hdf5'
 decoder_path = 'fraud_decoder_deep_optimal.hdf5'
 autoencoder_path = 'fraud_autoencoder_deep_optimal.hdf5'
+msqe_plot_path = 'fraud_ae_msqe.png'
+aerec_plot_path = 'fraud_ae_rec.png'
 
 # prepare input data
-x_train = pd.read_pickle(train_data_path)
-x_test = pd.read_pickle(test_data_path)
+x_train = pd.read_pickle(train_data_path).to_numpy()
+x_test = pd.read_pickle(test_data_path).to_numpy()
 
 # load model
 encoder = load_model(encoder_path, custom_objects={'iRprop_': iRprop_})
@@ -28,36 +30,50 @@ decoder.compile(optimizer=iRprop_(), loss='mean_squared_error')
 autoencoder.compile(optimizer=iRprop_(), loss='mean_squared_error')
 
 # encode and decode some data
-# note that we take them from the *test* set
-encoded_data = encoder.predict(x_test)
-decoded_data = decoder.predict(encoded_data)
+enc_train = encoder.predict(x_train)
+y_train = decoder.predict(enc_train)
+enc_test = encoder.predict(x_test)
+y_test = decoder.predict(enc_test)
 
-print('MSQE = ', K.eval(K.mean(mean_squared_error(tf.convert_to_tensor(x_test), tf.convert_to_tensor(decoded_data.astype(np.float64))))))
-print('XENT = ', K.eval(K.mean(binary_crossentropy(tf.convert_to_tensor(x_test), tf.convert_to_tensor(decoded_data.astype(np.float64))))))
+sqe_train = np.mean(np.square(x_train - y_train), axis = -1)
+sqe_test = np.mean(np.square(x_test - y_test), axis = -1)
+
+print('MSQE(train, test) = ', np.mean(sqe_train), np.mean(sqe_test))
+#print('XENT(train, test) = ',
+#        K.eval(K.mean(binary_crossentropy(tf.convert_to_tensor(x_train), tf.convert_to_tensor(y_train.astype(np.float64))))),
+#        K.eval(K.mean(binary_crossentropy(tf.convert_to_tensor(x_test), tf.convert_to_tensor(y_test.astype(np.float64))))))
+
+# Visualise MSQE using boxplots
+
+boxplots = {'Train': sqe_train, 'Test': sqe_test}
+fig, ax = plt.subplots()
+ax.set_xticklabels(boxplots.keys())
+plt.savefig(msqe_plot_path)
+plt.show()
 
 # Visualize the reconstructed encoded representations
-
+'''
 n = 10  # how many events we will display
 plt.figure(figsize=(20, 20), dpi=300)
 for i in range(n):
     # display original
-    ax = plt.subplot(3, n, i + 1)
-    plt.imshow(x_test.iloc[i].values.reshape(3, 11))
+    ax2 = plt.subplot(3, n, i + 1)
+    plt.imshow(x_test[i].reshape(3, 11))
     plt.gray()
-    ax.set_axis_off()
+    ax2.set_axis_off()
 
     # display encoded data
-    ax = plt.subplot(3, n, i + 1 + n)
-    plt.imshow(encoded_data[i].reshape(1, 3))
+    ax2 = plt.subplot(3, n, i + 1 + n)
+    plt.imshow(enc_test[i].reshape(1, 3))
     plt.gray()
-    ax.set_axis_off()
+    ax2.set_axis_off()
 
     # display reconstruction
-    ax = plt.subplot(3, n, i + 1 + 2*n)
-    plt.imshow(decoded_data[i].reshape(3, 11))
+    ax2 = plt.subplot(3, n, i + 1 + 2*n)
+    plt.imshow(y_test[i].reshape(3, 11))
     plt.gray()
-    ax.set_axis_off()
-
+    ax2.set_axis_off()
+plt.savefig(aerec_plot_path)
 plt.show()
-
+'''
 K.clear_session()
